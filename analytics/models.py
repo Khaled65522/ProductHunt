43d1356ca105aaf9dab5fc74b95b6d676e43137c
Mahmuda <mahmudasultana1420@gmail.com>
2024-11-24 23:09:29 +0600

    created report model

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
+from products.models import Product
 
 # Create your models here.
+class Report(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reports')
+    reported_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Report'
+        verbose_name_plural = 'Reports'
+        indexes = [models.Index(fields=['product', 'reported_by'])]
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.product.name} reported by {self.reported_by.first_name}"
