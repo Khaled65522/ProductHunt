f42aabff14456d597ed0614b1ff3127ef415d113
Lamia <lamiabristy31@gmail.com>
2024-11-28 00:16:40 +0600

    created product model

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("products/", views.all_products, name="all_products"),
+    path('create-product/', views.create_product, name="create_product"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import  *
+from .forms import *
 
 # Create your views here.
 def all_products(request):
-    return HttpResponse("All Products will be displayed here.")
+    #return HttpResponse("All Products will be displayed here.")
+    #request.method = "GET"
+    products = Product.objects.all()
+    return render(request, 'product.html', {'products': products})
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        form = ProductForm()
+    return render(request, 'addproduct.html', {'form': form})
