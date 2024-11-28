8ba85762e89c012a28d4671ce070a10ecf0d47c9
Lamia <lamiabristy31@gmail.com>
2024-11-28 14:28:23 +0600

    product model complete

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("products/", views.all_products, name="all_products"),
     path('create-product/', views.create_product, name="create_product"),
-    path('update-product/<int:p_id>/', views.update_product, name="update_product"),
+    path('detail-product/<int:id>', views.detail_product, name="detail_product"),
+    path('update-product/<int:id>', views.update_product, name="update_product"),
+    path('delete-product/<int:id>', views.delete_product, name="delete_product"),
 ]
